40b3fb89b01e08333b8c918dd8638db933dc61b7
Yuchen Zhang <yuchenzhang@me.com>
2012-03-28 12:40:33 +0200

    ror application initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialMenuServer::Application.config.secret_token = 'fc768627cac88e1b1c25e09ecf94c38b7963b0e1df039b740249a023021b53f5032f034ea00018e1eda17d2bb27998e513189d588f2bc37a8799ccc82210820b'
