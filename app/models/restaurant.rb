0e57bde2386377b317c95423f75cad54988c8d30
Yuchen Zhang <yuchenzhang@me.com>
2012-04-03 14:39:25 +0200

    json interface including dishes

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,6 @@ class Restaurant < ActiveRecord::Base
   has_many :dishes
   validates_uniqueness_of :uuid, :on => :create, :message => "has already been taken"
   def to_json(options = nil)
-    super(:except => [:id, :created_at, :updated_at,:uuid, :city_id], :include => {:city => {:only => :name}, :dishes => {:methods => :to_json} })
+    super(:except => [:id, :created_at, :updated_at,:uuid, :city_id], :include => {:city => {:only => :name}, :dishes => {:only => [:name, :price, :description]} })
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first) 
 
 eindhoven,rotterdam,amsterdam,utrecht,denhaag = City.create([{ name: 'Eindhoven' }, { name: 'Rotterdam' }, {name: 'Amsterdam'},{name: 'Utrecht'}, {name: 'Den Haag'}])
+dishes = [
+  ['Thai Chicken', 5.50, 'Kipfilet met groeten en thaise curry - cocossaus'],
+  ['Kon Bon Chicken', 5.50, 'Kipfilet met verse kruiden en cashewnoot in licht prikante saus'],
+  ['King Doo Chicken',5.50, 'Kipfilet in een zoetzure saus met ananas'],
+  ['Thai Beef', 6.50, 'Heerlijke biefstuk met thaise curry - cocossaus'],
+  ['Tokyo Beef',6.50, 'Biefstuk in Japanse sojasaus'],
+  ['Garlic Seafood', 6.00, 'Mix van zeevruchten, groenten in verse knoflooksaus'],
+  ['Tropical Prawn', 6.50, 'Garnalen met ananas, tomaten, uien in fruitige zoetezuresaus'],
+  ['Tjap Tjoy', 4.50, 'Mix van seizongroenten']
+]
+
 soho_eindhoven = Restaurant.create(:name => "soho Eindhoven", 
 :uuid => UUID.new.generate, 
 :latitude => 51.436126, :longitude => 5.488164, 
@@ -15,6 +26,9 @@ soho_eindhoven = Restaurant.create(:name => "soho Eindhoven",
 :address_line_2 => "1234 AA")
 1.upto(11) do |i|
   soho_eindhoven.tables.create!(:number => i,:uuid => UUID.new.generate)
+end
+dishes.each do |d|
+  soho_eindhoven.dishes.create!(:name => d[0],:price => d[1], :description => d[2])
 end
 soho_utrecht = Restaurant.create(:name => "soho Utrecht",
 :uuid => UUID.new.generate,
@@ -25,6 +39,9 @@ soho_utrecht = Restaurant.create(:name => "soho Utrecht",
 1.upto(11) do |i|
   soho_utrecht.tables.create!(:number => i,:uuid => UUID.new.generate)
 end
+dishes.each do |d|
+  soho_utrecht.dishes.create!(:name => d[0],:price => d[1], :description => d[2])
+end
 soho_rotterdam = Restaurant.create(:name => "soho Rotterdam",
 :uuid => UUID.new.generate,
 :latitude => 51.917168, :longitude => 4.493408,
@@ -33,6 +50,9 @@ soho_rotterdam = Restaurant.create(:name => "soho Rotterdam",
 :address_line_2 => "1234 AA")
 1.upto(11) do |i|
   soho_rotterdam.tables.create!(:number => i,:uuid => UUID.new.generate)
+end
+dishes.each do |d|
+  soho_rotterdam.dishes.create!(:name => d[0],:price => d[1], :description => d[2])
 end
 soho_amsterdam = Restaurant.create(:name => "soho Amsterdam",
 :uuid => UUID.new.generate,
@@ -43,6 +63,9 @@ soho_amsterdam = Restaurant.create(:name => "soho Amsterdam",
 1.upto(11) do |i|
   soho_amsterdam.tables.create!(:number => i,:uuid => UUID.new.generate)
 end
+dishes.each do |d|
+  soho_amsterdam.dishes.create!(:name => d[0],:price => d[1], :description => d[2])
+end
 soho_denhaag = Restaurant.create(:name => "soho Den Haag",
 :uuid => UUID.new.generate,
 :latitude => 52.062623, :longitude => 4.312134,
@@ -51,4 +74,7 @@ soho_denhaag = Restaurant.create(:name => "soho Den Haag",
 :address_line_2 => "1234 AA")
 1.upto(11) do |i|
   soho_denhaag.tables.create!(:number => i,:uuid => UUID.new.generate)
+end
+dishes.each do |d|
+  soho_denhaag.dishes.create!(:name => d[0],:price => d[1], :description => d[2])
 end
